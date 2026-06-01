cd6eb7c9a3033fe5ec7c238f873d837061ab489d
Arisgav <amgavarrete@uamv.edu.ni>
2026-06-01 13:31:27 -0600

    leccion3

--- src/main/java/com/tuempresa/facturacion/calculadores/CalculadorSiguienteNumeroParaAnyo.java
+++ src/main/java/com/tuempresa/facturacion/calculadores/CalculadorSiguienteNumeroParaAnyo.java
@@ -1,24 +1,33 @@
 package com.tuempresa.facturacion.calculadores;
 
-import javax.persistence.*;
-import org.openxava.calculators.*;
-import org.openxava.jpa.*;
-import lombok.*;
+import javax.persistence.Query;
 
-public class CalculadorSiguienteNumeroParaAnyo
-        implements ICalculator { // Un calculador tiene que implementar ICalculator
+import org.openxava.calculators.ICalculator;
+import org.openxava.jpa.XPersistence;
 
-    @Getter @Setter
-    int anyo; // Este valor se inyectará antes de calcular
+import lombok.Getter;
+import lombok.Setter;
 
-    public Object calculate() throws Exception { // Hace el cálculo
-        Query query = XPersistence.getManager() // Una consulta JPA
-                .createQuery("select max(f.numero) from Factura f where f.anyo = :anyo"); // La consulta devuelve
-        // el número de factura máximo del año indicado
-        query.setParameter("anyo", anyo); // Ponemos el año inyectado como parámetro de la consulta
+public class CalculadorSiguienteNumeroParaAnyo implements ICalculator {
+
+    @Getter
+    @Setter
+    private int anyo;
+
+    @Override
+    public Object calculate() throws Exception {
+
+        Query query = XPersistence.getManager()
+                .createQuery(
+                        "select max(f.numero) " +
+                                "from Factura f " +
+                                "where f.anyo = :anyo"
+                );
+
+        query.setParameter("anyo", anyo);
+
         Integer ultimoNumero = (Integer) query.getSingleResult();
-        return ultimoNumero == null ? 1 : ultimoNumero + 1; // Devuelve el último número
-        // de factura del año + 1 o 1 si no hay último número
-    }
 
+        return ultimoNumero == null ? 1 : ultimoNumero + 1;
+    }
 }
--- src/main/java/com/tuempresa/facturacion/modelo/Factura.java
+++ src/main/java/com/tuempresa/facturacion/modelo/Factura.java
@@ -1,34 +1,52 @@
 package com.tuempresa.facturacion.modelo;
 
 import java.time.*;
+import java.util.*;
+
 import javax.persistence.*;
+
 import org.hibernate.annotations.GenericGenerator;
 import org.openxava.annotations.*;
 import org.openxava.calculators.*;
+
+import com.tuempresa.facturacion.calculadores.CalculadorSiguienteNumeroParaAnyo;
+
 import lombok.*;
 
-@Entity @Getter @Setter
+@Entity
+@Getter
+@Setter
 public class Factura {
 
     @Id
-    @GeneratedValue(generator="system-uuid")
+    @GeneratedValue(generator = "system-uuid")
     @Hidden
-    @GenericGenerator(name="system-uuid", strategy="uuid")
-    @Column(length=32)
-    String oid;
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(length = 32)
+    private String oid;
 
-    @Column(length=4)
-    @DefaultValueCalculator(CurrentYearCalculator.class) // Año actual
-    int anyo;
+    @Column(length = 4)
+    @DefaultValueCalculator(CurrentYearCalculator.class)
+    private int anyo;
 
-    @Column(length=6)
-    int numero;
+    @Column(length = 6)
+    @DefaultValueCalculator(
+            value = CalculadorSiguienteNumeroParaAnyo.class,
+            properties = @PropertyValue(name = "anyo")
+    )
+    private int numero;
 
     @Required
-    @DefaultValueCalculator(CurrentLocalDateCalculator.class) // Fecha actual
-    LocalDate fecha;
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class)
+    private LocalDate fecha;
 
     @TextArea
-    String observaciones;
+    private String observaciones;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Cliente cliente;
+
+    @ElementCollection
+    @ListProperties("producto.numero, producto.descripcion, cantidad")
+    private Collection<Detalle> detalles;
 }
